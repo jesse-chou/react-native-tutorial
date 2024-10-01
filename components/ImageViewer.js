98556542d464365bf2bcd301f7da5d0eb4040a24
Jesse Chou <jessechou7@gmail.com>
2024-10-01 19:57:34 -0400

    finished tutorial up to gestures

---
+++ components/ImageViewer.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Image } from "react-native";
+export default function ImageViewer({ placeholderImage, selectedImage }) {
+  const imageSource = selectedImage ? { uri: selectedImage } : placeholderImage;
+  return <Image source={imageSource} style={styles.image}></Image>;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
